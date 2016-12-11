3afbe95a605254dd8e3fc4064a666e9745538afb
Luisa Quispe O <lqo202@nyu.edu>
2016-12-11 17:57:50 -0500

    change

--- model/Classes/Recurrent/train2.py
+++ model/Classes/Recurrent/train2.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import time
 import datetime
-import data_helpers2
+import data_helpers2 as data_helpers
 from rec_cbof2 import LSTM_CBOW
 from tensorflow.contrib import learn
 from sys import exit
