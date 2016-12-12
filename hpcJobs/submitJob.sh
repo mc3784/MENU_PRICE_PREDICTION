165fbb3dc5679529e5e1ec7e018ac3f07f03770d
Michele Ceru <mc3784@login-0-0.local>
2016-12-12 17:58:13 -0500

    call with directory as argument

--- hpcJobs/submitJob.sh
+++ hpcJobs/submitJob.sh
@@ -1,21 +1,22 @@
 #PBS -l nodes=1:ppn=1:gpus=1
 #PBS -l walltime=8:00:00
 #PBS -l mem=32GB
-#PBS -N continous_W2V
+#PBS -N Continuous_W2V
 #PBS -M mc3784@nyu.edu
 #PBS -m b -m e -m a -m abe
 #PBS -j oe
 
 module purge
 
-SRCDIR=$HOME/NLP/MENU_PRICE_PREDICTION/model/Continuous/W2V/
+SRCDIR=$HOME/NLP/MENU_PRICE_PREDICTION/model/Continuous/W2V
 RUNDIR=$SCRATCH/MENU/run-${PBS_JOBID/.*}
 mkdir -p $RUNDIR
 
 cd $PBS_O_WORKDIR
 cp -R $SRCDIR/*.py $RUNDIR
+cp submitJob.sh $RUNDIR
 cd $RUNDIR
 
-module load tensorflow/python2.7/20160418
+module load tensorflow/python2.7/20161029
 module load nltk/3.0.2
-python train.py
+python train.py --data_dir "/home/mc3784/NLP/MENU_PRICE_PREDICTION/data" --word2vec "/home/mc3784/NLP/MENU_PRICE_PREDICTION/GoogleNews-vectors-negative300.bin"
